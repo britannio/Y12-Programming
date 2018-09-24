b0383ade8855537be0248eb24328d5ea8684c1af
britannio <britanniojarrett@gmail.com>
2018-09-24 22:05:59 +0100

    added fizz buzz

---
+++ Fundamentals 1/short_hand_fizz_buzz.py
@@ -0,0 +1,5 @@
+'''
+One line Fizz buzz program
+'''
+
+[print("Fizz buzz") if x%3 == 0 and x%5 == 0 else print("Fizz") if x % 3 == 0 else print("Buzz") if x%5 == 0 else print(str(x)) for x in range(1, 100 + 1)]
